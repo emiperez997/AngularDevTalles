29617dea212a239629ffc4edb728b47b9f0b2ac8
emi.perez997 <emi.perez997@gmail.com>
2024-10-29 19:56:49 +0000

    [ADD] Seccion 3

---
+++ seccion_3/src/main.ts
@@ -0,0 +1,7 @@
+import './style.css'
+// import './topics/01-basic-types.ts'
+import './topics/02-object-interface.ts'
+
+document.querySelector<HTMLDivElement>('#app')!.innerHTML = `
+  <h1> Hola mundo </h1>
+`
